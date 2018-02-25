dd0c35273b5d9015a1933b9228bf4f8c4563bfb3
David Mandelbaum <dasmandelbaum@gmail.com>
2018-02-24 23:36:49 -0500

    test1

---
+++ classFiles/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#David Mandelbaum
+#Last updated: 2.24.18
+echo '####################################################################################################################################################################################################'
+echo 'BEGINNING PROGRAM'
+./server 8181 . 8 10 HPIC &
+./client localhost 8181 /index.html &
+./client localhost 8181 /index.html &
+./client localhost 8181 /index.html &
+./client localhost 8181 /nigel.jpg &
+./client localhost 8181 /nigel.jpg &
+./client localhost 8181 /nigel.jpg &
+./client localhost 8181 /nigel.jpg &
+./client localhost 8181 /index.html &
+./client localhost 8181 /index.html &
+./client localhost 8181 /index.html &
+./client localhost 8181 /index.html &
+./client localhost 8181 /index.html &
+echo 'ENDING PROGRAM' 
+echo '####################################################################################################################################################################################################' 
